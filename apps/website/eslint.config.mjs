0ada013f64208e46913b245ff3e6664d120a2350
Zesan <zesanrahim@gmail.com>
2025-05-10 21:05:32 -0400

    feat(git): update gitignore at root level

--- apps/website/eslint.config.mjs
+++ apps/website/eslint.config.mjs
@@ -1,6 +1,9 @@
 import nx from '@nx/eslint-plugin';
 import baseConfig from '../../eslint.config.mjs';
 
+
+globalThis.structuredClone = globalThis.structuredClone || ((obj) => JSON.parse(JSON.stringify(obj)));
+
 export default [
   ...baseConfig,
   ...nx.configs['flat/react'],
